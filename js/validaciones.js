9ddd0dff389969242855afd53f82767d3bdef8ff
Sergio Capera <sergio_capera@hotmail.com>
2021-10-01 17:50:25 -0500

    Sprint 3 terminado

--- js/validaciones.js
+++ js/validaciones.js
@@ -38,7 +38,55 @@ function validar_contrasena(string){
     
 }
 
+let registros = [];
 
-/*module.exports.validar_nombre_usuario = validar_nombre_usuario;
+function agregarRegistro(){
+
+    function Usuario(usuario, edad, contrasena){
+        this.usuario = usuario;
+        this.edad = edad;
+        this.contrasena = contrasena;
+    }
+
+    var rNombre = document.getElementById('dato_nombre_usuario').value;
+    var rEdad = document.getElementById('dato_edad_usuario').value;
+    var rConstrasena = document.getElementById('dato_contrasena').value;
+    
+    Arreglo = new Usuario(rNombre,rEdad,rConstrasena)
+    
+    document.getElementById('dato_nombre_usuario').value = "";
+    document.getElementById('dato_edad_usuario').value = "";
+    document.getElementById('dato_contrasena').value = "";
+
+    registros.push(Arreglo);
+
+    var organizado = OrdenarArreglo();
+
+    //console.log(registros)
+    console.log(organizado)
+
+}
+
+function OrdenarArreglo(arreglo){
+
+    var arreglo = registros;
+
+    arreglo.sort((a,b)=>{
+        if(a.edad < b.edad){
+            return-1;
+        }else if(a.edad > b.edad){
+            return 1;
+        }else{
+            return 0;
+        }
+    })
+
+    return arreglo;
+}
+
+module.exports.validar_nombre_usuario = validar_nombre_usuario;
 module.exports.validar_contrasena = validar_contrasena;
-module.exports.validar_edad_usuario = validar_edad_usuario;*/
+module.exports.validar_edad_usuario = validar_edad_usuario;
+module.exports.registros = registros;
+module.exports.OrdenarArreglo = OrdenarArreglo;
+module.exports.agregarRegistro = agregarRegistro;
